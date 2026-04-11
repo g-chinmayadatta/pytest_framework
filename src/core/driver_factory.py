9f607e43a8c5e3b159fd1ab5082504568811e4bd
Chinmaya <chinmaya.ucen@gmail.com>
2026-04-11 12:19:38 +0530

    update the chrome headless setting

--- src/core/driver_factory.py
+++ src/core/driver_factory.py
@@ -26,6 +26,7 @@ class DriverFactory:
                 options.add_argument("--headless=new")
                 options.add_argument("--no-sandbox")
                 options.add_argument("--disable-dev-shm-usage")
+                options.add_argument("--disable-gpu")
 
             options.add_argument("--start-maximized")
             options.add_argument("--disable-notifications")
